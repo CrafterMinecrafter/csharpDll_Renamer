95bdb3c33e33a0eb610822230653226066d60e37
Даниил <67530305+CrafterMinecrafter@users.noreply.github.com>
2021-02-27 02:37:03 +0200

    1

--- csharpDll_Renamer/Program.cs
+++ csharpDll_Renamer/Program.cs
@@ -9,16 +9,24 @@ internal class Program
 		Console.ForegroundColor = ConsoleColor.Red;
 		Console.WriteLine("Created by CrafterMinecrafter | https://github.com/CrafterMinecrafter");
 		Console.ForegroundColor = ConsoleColor.DarkRed;
-		foreach (string text in dlls)
+		
+		if (dlls.Length == 0)
 		{
-			Directory.CreateDirectory(Path.Combine(Path.GetDirectoryName(text), "renamed"));
-			string assemblyName = Assembly.ReflectionOnlyLoad(File.ReadAllBytes(text)).FullName.Split(new char[]
+			Console.WriteLine("Please, put dlls in exe");
+		}
+		else
+		{
+			foreach (string text in dlls)
 			{
-				','
-			})[0];
-			string destFileName = Path.Combine(Path.GetDirectoryName(text), "renamed\\" + assemblyName + ".dll");
-			File.Copy(text, destFileName);
-			Console.WriteLine(text + " Copied to " + destFileName);
+				Directory.CreateDirectory(Path.Combine(Path.GetDirectoryName(text), "renamed"));
+				string assemblyName = Assembly.ReflectionOnlyLoad(File.ReadAllBytes(text)).FullName.Split(new char[]
+				{
+					','
+				})[0];
+				string destFileName = Path.Combine(Path.GetDirectoryName(text), "renamed\\" + assemblyName + ".dll");
+				File.Copy(text, destFileName);
+				Console.WriteLine(text + " Copied to " + destFileName);
+			}
 		}
 		Console.ReadLine();
 	}
